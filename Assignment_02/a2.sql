ee2e9ac417468d898fd1128a2df8440a99238d25
Kimberly Crevecoeur <kimberwolves@gmail.com>
2020-09-16 18:58:34 -0400

    completed more problems

--- Assignment_02/a2.sql
+++ Assignment_02/a2.sql
@@ -1,7 +1,8 @@
 /* Kimberly Crevecoeur 
 
 worked with:
-
+michael abbott
+Zunaeed S
 */
 -- Section 1
 
@@ -78,10 +79,29 @@ ORDER BY pid;
 
 
 
--- 1.4
+-- 1.4.a
+\qecho '1.4.a'
+Select DISTINCT p1.pid, p1.name
+FROM person p1, worksfor w, personskill ps
+WHERE p1.pid = w.pid and w.cname = 'IBM' and w.salary =
+                                            (Select max(salary)
+    FROM (SELECT w2.Salary
+        FROM worksfor w2
+        WHERE w2.cname = 'IBM')x);
+
+
+\qecho '1.4.b'
+Select DISTINCT p1.pid, p1.name
+FROM person p1, worksfor w, personskill ps
+WHERE p1.pid = w.pid and w.cname = 'IBM' and w.salary =
+                                            (Select max(salary)
+    FROM (SELECT w2.Salary
+        FROM worksfor w2
+        WHERE w2.cname = 'IBM')x);
+
 
 --1.5
-\qecho 1.5
+\qecho '1.5'
 SELECT DISTINCT c.cname, p1.pid, p1.name
 FROM person p1, company c, worksfor w
 WHERE p1.pid = w.pid and w.cname = c.cname and w.salary  in
@@ -123,10 +143,34 @@ WHERE (p1.pid not in (select k2.pid1
 )
 order by pid;
 
+-- 1.7
+\qecho '1.7'
+    SELECT j.skill
+    FROM personskill j
+EXCEPT
+    (select j2.skill
+    FROM person p2, worksfor w, personskill j2
+    WHERE p2.pid = w.pid and p2.pid = j2.pid and w.cname in ('Yahoo', 'Netflix')
+);
+
+-- 1.8
+\qecho '1.8'
+
+SELECT js1.skill, js2.skill
+FROM jobskill js1, jobskill js2
+WHERE (js1.skill = js2.skill) or
+    ( Select ps1.pid
+    from personskill ps1
+    where ps1.skill = js1.skill
+        ) =
+    ALL
+        (Select ps2.pid
+    from personskill ps2
+    where ps2.skill = js2.skill);
 /*
 person: pid, name, city, birthyear
 knows: pid1, pid2, PK -- (pid1, pid2)
-company: cname, city, PK -- (cname, city)
+company: cname, city, PK -- (cname, city)3
 worksfor: pid, cname, salary
 jobskill: skill, PK-- skill
 personskill: pid, skill
@@ -134,6 +178,8 @@ personskill: pid, skill
 */
 
 -- 2.1.a
+\qecho
+'2.1.a'
 DROP VIEW IF EXISTS SalaryAbove50000;
 CREATE VIEW SalaryAbove50000
 AS
@@ -147,6 +193,8 @@ from SalaryAbove50000;
 
 
 -- 2.1.b
+\qecho '2.1.b'
+
 DROP VIEW IF EXISTS IBMEmployee;
 CREATE VIEW IBMEmployee
 AS
@@ -158,6 +206,8 @@ select *
 from IBMEmployee;
 
 -- 2.1.c
+\qecho '2.1.c'
+
     Select p.pid, p.name
     from person p, worksfor w
     where p.pid = w.pid and w.cname = 'Apple'
@@ -171,6 +221,7 @@ EXCEPT
 
 -- 2.2.a
 \qecho '2.2.a'
+
 DROP FUNCTION IF EXISTS SalaryAbove;
 CREATE FUNCTION SalaryAbove(amount INTEGER) RETURNS TABLE
 (pid INTEGER, name TEXT, city TEXT, birthyear INTEGER) AS
@@ -193,32 +244,74 @@ SELECT *
 FROM SalaryAbove(70000);
 
 -- 2.2.b
+
+
 \qecho '2.2.b'
-DROP FUNCTION IF EXISTS KnowsEmployeeAtCompany;
-CREATE FUNCTION KnowsEmployeeAtCompany(workname TEXT) RETURNS TABLE
-(pid INTEGER) AS
+drop function if exists knowsemployeeatcompany;
+CREATE function
+ KnowsEmployeeAtCompany
+(workname TEXT) RETURNS TABLE
+(pid INTEGER, kpid INTEGER) AS
 $$
-SELECT distinct p1.pid
+SELECT distinct p1.pid, k.pid2
 FROM person p1, knows k, person p2, worksfor w
 WHERE k.pid1 = p1.pid and k.pid2 = p2.pid and w.pid = p2.pid and w.cname = workname;
 $$   LANGUAGE  SQL;
 
 \qecho 'People who know someone who works at Yahoo'
-SELECT *
-FROM KnowsEmployeeAtCompany('Yahoo');
+SELECT c.pid
+FROM KnowsEmployeeAtCompany('Yahoo') c;
 
 \qecho 'People who know someone who works at Google'
-SELECT *
-FROM KnowsEmployeeAtCompany('Google');
+SELECT c.pid
+FROM KnowsEmployeeAtCompany('Google') c;
 
 \qecho 'People who know someone who works at Amazon'
-SELECT *
-FROM KnowsEmployeeAtCompany('Amazon');
+SELECT c.pid
+FROM KnowsEmployeeAtCompany('Amazon') c;
 
 --2.2.c
+
+
+SELECT DISTINCT w2.salary, c.cname, p.pid
+FROM worksfor w, company c, person p, company c1, worksfor w2
+WHERE p.pid = w.pid
+    AND w.cname = c.cname
+    AND p.pid IN (SELECT x.pid
+    FROM SalaryAbove(w2.salary)x) -- persons salary is above
+    AND p.pid IN (Select y.pid
+    FROM KnowsEmployeeAtCompany(c1.cname) y
+    WHERE c1.cname != c.cname -- making sure they both don't work at the same company
+        AND y.kpid NOT IN (SELECT t.pid
+        FROM SalaryAbove(w2.salary)t));
+
+
 /*
 Select w.salary, w.cname, p1.pid
 FROM person p1, worksfor w, worksfor w2, company c
 WHERE p1.pid = w2.pid and w2.cname = w.cname
     and  
+
+
+*/
+
+/*
+Select DISTINCT w1.salary, c.cname, p1.pid
+FROM worksfor w1, company c, company c2, person p1, worksfor w2
+WHERE p1.pid in (Select sa.pid
+    FROM SalaryAbove(w1.salary) sa)
+    AND p1.pid = w2.pid and w2.cname = c.cname
+
+    AND p1.pid in 
+        (select kn.pid
+    from KnowsEmployeeAtCompany(c2.cname)kn
+    WHERE 
+     c2.cname != c.cname
+        AND kn.pid not in (Select sa2.pid
+        from SalaryAbove(w2.salary) sa2));
+*/
+/*
+SELECT DISTINCT w2.salary, c1.name, p.pid 
+FROM worksfor w1, worksfor w2, company c1, company c2, person p 
+WHERE p.pid = w1.pid and w1.cname = c1.cname AND p.pid 
 */
